9febbcf9ad9c94878e7049e606e1da38b86960c0
Darshan Bajgain <darshanbajgain90@gmail.com>
2024-08-03 20:33:21 +0545

    refactored: CORS Problem configured

--- src/main/java/com/darshan/userauthapp/UserauthappApplication.java
+++ src/main/java/com/darshan/userauthapp/UserauthappApplication.java
@@ -1,13 +1,29 @@
 package com.darshan.userauthapp;
 
+import org.springframework.lang.NonNull;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class UserauthappApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserauthappApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			public void addCorsMappings ( @NonNull CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedMethods("*")
+						.allowedOrigins("http://localhost:5173");
+
+			}
+		};
 	}
 
 }
